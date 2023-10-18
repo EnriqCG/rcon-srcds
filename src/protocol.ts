82c6cee5ea5bcb875e031a45862cdcb01c1a516a
Markus Adrario <Mozilla@adrario.de>
2023-10-18 16:33:50 +0200

    Add ID for termination packets
    This adds a static ID for empty packets, that are sent to find the end of a multipacket response

--- src/protocol.ts
+++ src/protocol.ts
@@ -11,7 +11,8 @@ const protocol = Object.freeze({
     SERVERDATA_RESPONSE_VALUE: 0x00,    // Response of SERVERDATA_EXECCOMMAND
 
     ID_AUTH: 0x999,
-    ID_REQUEST: 0x123
+    ID_REQUEST: 0x123,
+    ID_TERM:0x777
 })
 
 export default protocol
